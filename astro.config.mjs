9b7efdfba44a5b4ff60e6208a942b8c07aeae992
Tim Veletta <tim.veletta@mechanicalrock.io>
2023-03-07 15:34:53 +0800

    changing to static

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,18 @@
 import { defineConfig } from 'astro/config';
 
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
-import vercel from "@astrojs/vercel/serverless";
+import vercel from '@astrojs/vercel/static';
 
 // https://astro.build/config
 
 // https://astro.build/config
-import react from "@astrojs/react";
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
-  output: "server",
-  adapter: vercel()
+	integrations: [tailwind(), react()],
+	output: 'static',
+	adapter: vercel(),
 });
