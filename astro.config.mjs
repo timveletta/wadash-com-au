4c7407c88332d07b88fd7a685d8b2c4234e7dd04
Tim Veletta <tim.veletta@mechanicalrock.io>
2023-03-07 16:43:07 +0800

    local build update

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,6 +12,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://wadash.com.au',
 	integrations: [tailwind(), react()],
 	output: 'static',
 	adapter: vercel(),
