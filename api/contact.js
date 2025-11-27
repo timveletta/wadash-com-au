1c2530882c243dff116e59f7c9a9bddf9d2d02f3
Tim Veletta <480823+timveletta@users.noreply.github.com>
2025-11-27 17:10:00 +0800

    Update email handler

--- api/contact.js
+++ api/contact.js
@@ -1,38 +1,44 @@
-import mail from '@sendgrid/mail';
-mail.setApiKey(process.env.SENDGRID_API_KEY);
+import { Resend } from "resend";
 
-const msg = {
-	to: 'contact@wadash.com.au', // Change to your recipient
-	from: 'contact@em3447.wadash.com.au', // Change to your verified sender
-	subject: 'WADash.com.au Contact Form Submission',
-};
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 function formatMessage(name, phoneNumber, postCode, subject, message) {
-	return `
+  return `
 	<h1>WADash.com.au Contact Form Submission</h1>
 	<p><strong>Name:</strong> ${name}</p>
 	<p><strong>Phone Number:</strong> ${phoneNumber}</p>
 	<p><strong>Post Code:</strong> ${postCode}</p>
-	<p><strong>Subject:</strong> ${subject || 'None'}</p>
+	<p><strong>Subject:</strong> ${subject || "None"}</p>
 	<p><strong>Message:</strong> ${message}</p>
   `;
 }
 
 export default async function handler(request, response) {
-	const { name, subject, phoneNumber, postCode, message } = JSON.parse(
-		request.body
-	);
-	try {
-		const result = await mail.send({
-			...msg,
-			html: formatMessage(name, phoneNumber, postCode, subject, message),
-		});
-		response.status(result[0].statusCode).json({
-			body: 'Message sent successfully!',
-		});
-	} catch (error) {
-		response.status(500).json({
-			body: 'Message failed to send.',
-		});
-	}
+  const { name, subject, phoneNumber, postCode, message } = JSON.parse(
+    request.body
+  );
+  try {
+    const { data, error } = await resend.emails.send({
+      from: "WADash Contact <contact@velettaliwedding.com>",
+      to: ["timothy.veletta@gmail.com"], //'contact@wadash.com.au'],
+      subject: "WADash.com.au Contact Form Submission",
+      html: formatMessage(name, phoneNumber, postCode, subject, message),
+    });
+
+    if (error) {
+      console.error(error);
+      return response.status(500).json({
+        body: "Message failed to send.",
+      });
+    }
+
+    response.status(200).json({
+      body: "Message sent successfully!",
+    });
+  } catch (error) {
+    console.error(error);
+    response.status(500).json({
+      body: "Message failed to send.",
+    });
+  }
 }
