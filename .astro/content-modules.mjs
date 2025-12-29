18335ce2eed1ec1c83faf9e72bf694d1f05d6018
Tim Veletta <480823+timveletta@users.noreply.github.com>
2025-12-29 20:27:37 +0800

    Add JSON structured data

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,4 +1,6 @@
 
 export default new Map([
-["services/hot-water-systems.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=services%2Fhot-water-systems.mdx&astroContentModuleFlag=true")]]);
+["services/hot-water-systems.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=services%2Fhot-water-systems.mdx&astroContentModuleFlag=true")],
+["services/hot-water-systems copy.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=services%2Fhot-water-systems+copy.mdx&astroContentModuleFlag=true")],
+["services/emergency-plumbing.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=services%2Femergency-plumbing.mdx&astroContentModuleFlag=true")]]);
 		
